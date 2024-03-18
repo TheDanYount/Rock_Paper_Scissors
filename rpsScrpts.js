091c53763e8b96c60733b662680d1ce1c3d32e71
Dan Yount <158097100+TheDanYount@users.noreply.github.com>
2024-03-18 12:33:15 -0700

    Wrote getComputerChoice function

--- rpsScrpts.js
+++ rpsScrpts.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let numberValue = Math.floor(Math.random()*3)
+    switch(numberValue) {
+        case 0:
+        return "rock";
+        case 1:
+        return "paper";
+        case 2:
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice())
+console.log(getComputerChoice())
+console.log(getComputerChoice())
+console.log(getComputerChoice())
+console.log(getComputerChoice())
+console.log(getComputerChoice())
